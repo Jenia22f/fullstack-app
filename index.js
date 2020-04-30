ea0d6dbcae33cddcc9b44e03203957fb73d2c9f6
Jenia Altukhov <ye.altukhov@gmail.com>
2020-04-30 16:45:31 +0300

    fix port

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const app = require('./app');
 const port = process.env.PORT || 5000;
 
 
-app.listen(port, ()=> console.log(`Server works on port : ${port}`));
+// app.listen(port, ()=> console.log(`Server works on port : ${port}`));
+
+const server = app.listen(process.env.PORT || 5000, function () {
+    const port = server.address().port;
+    console.log("Server is working on port " + port);
+});
