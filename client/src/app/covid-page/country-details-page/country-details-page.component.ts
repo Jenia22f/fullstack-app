8b6a0a3976bcc3c9ca4c42376933b3199e3186b8
Jenia Altukhov <ye.altukhov@gmail.com>
2020-07-24 13:45:27 +0300

    country page

---
+++ client/src/app/covid-page/country-details-page/country-details-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-details-page',
+  templateUrl: './country-details-page.component.html',
+  styleUrls: ['./country-details-page.component.css']
+})
+export class CountryDetailsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
